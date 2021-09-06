2a128025edc17f1cc588bbce24c00c42dda81843
Utkarsh <uparekh@commure.com>
2021-09-06 13:13:59 -0700

    first go program

---
+++ tutorial.go
@@ -0,0 +1,19 @@
+//define a package
+package main
+
+//import module
+import "fmt"
+
+//define entry point
+func main() {
+	fmt.Println("Hello World!")
+}
+
+// How to run
+// go run tutorial.go
+
+// about go
+// go is compiled language and pass the compile file to computer
+// To generate build/compiled file, run go build tutorial.go, and then run ./tutorial (if you're using mac))
+
+// That's it... this is all for first go program!!! yey!!
