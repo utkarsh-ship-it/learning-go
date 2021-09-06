81a0251965f0e3b80cc4bfa0fa972b0fdf475a9c
Utkarsh <uparekh@commure.com>
2021-09-06 15:52:38 -0700

    Arithmatic oprations

--- variables_and_datatypes/variables.go
+++ variables_and_datatypes/variables.go
@@ -15,6 +15,23 @@ func main() {
 	// name = "Tim"
 	// name = "Bill"
 	//var number uint8  = 260 (cannot use 260 (untyped int constant) as uint8 value in variable declaration (overflows)compilerNumericOverflow)
-	//var number uint16 = 260
+	//var number uint16 = 260 (Explicit declaration)
 	//fmt.Println("Hello World!", number)
+	// var number = 260.98 //(Implicit declaration, imiplicitly guesses what type of the variable)
+	// fmt.Println("Hello world!", number)
+
+	// this will give us type of number: use Printf
+	// fmt.Printf("%T\n", number)
+
+	//number := 6 (fastest and easiest way to declar a variable)
+	//fmt.Printf("%T\n", number)
+
+	//number = "Str" (can't do now, since it intantiated as int)
+
+	// create a variable but don't give it a value
+	// all of the types has a default value
+	var number uint64
+	var bl bool // default type false
+	fmt.Println(number, bl)
+
 }
